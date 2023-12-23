888170230fd42f9863aec5f65d2e937f396e3f3f
Grayson Shogan <graysontoddshogan@gmail.com>
2023-12-23 14:51:42 -0800

    Done

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -1,11 +1,38 @@
 import React from "react";
 import Figure from "./Figure";
+
+import styled, { keyframes } from "styled-components";
+
+const kf = keyframes`
+50% {
+    transform: scale(0.8);
+  }
+
+100% {
+  transform: scale(1);
+}
+`;
+
+const StyledCard = styled.div`
+  padding: 10px;
+  transform: scale(1.5);
+  animation: ${kf} 0.2s ease-in-out forwards;
+
+  &:hover {
+    background-color: ${(pr) => pr.theme.tertiaryColor};
+    h2 {
+      color: ${(props) => props.theme.primaryColor};
+      font-size: 2rem;
+    }
+  }
+`;
+
 export default function Card({ title, text, imageURL, date }) {
   return (
-    <div className="card">
+    <StyledCard className="card">
       <h2>{title}</h2>
       <p>{text}</p>
       <Figure imageURL={imageURL} caption={date} />
-    </div>
+    </StyledCard>
   );
 }
---
+++ frontend/theme/index.js
@@ -0,0 +1,8 @@
+export default {
+  primaryColor: "royalblue",
+  secondaryColor: "crimson",
+  tertiaryColor: "beige",
+  black: "black",
+  white: "white",
+  breakpointMobile: "{max-width: 550px}",
+};
